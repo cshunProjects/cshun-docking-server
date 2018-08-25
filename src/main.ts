2b0af0c5040a20babb0401923dbf3e65e8450ee7
heymind <heymind@outlook.com>
2018-08-25 21:55:03 +0800

    api 文档显示代码版本

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import {readFileSync} from 'fs';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const options = new DocumentBuilder()
     .setTitle('长顺产销对接系统后端接口')
-    .setVersion('0')
+    .setVersion(readFileSync('.git/refs/heads/master').toString().slice(0,7))
     .addBearerAuth('Authorization','header')
     .setSchemes(process.env['NODE_ENV']==='production' ? "https" :"http")
     .build();
