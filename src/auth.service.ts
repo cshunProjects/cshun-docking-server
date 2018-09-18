b339a571fa8b338411ff485bf138d8dc82410ec5
Heymind <swthink@live.cn>
2018-09-18 03:06:35 +0000

    dockerfile support

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
 
     async createToken(user:User) {
         const payload: IJwtPayload = { id: user.id };
-        return jwt.sign(payload, process.env['JWT_SECRET_KEY'], { expiresIn: 360000 });
+        return jwt.sign(payload, process.env['JWT_SECRET_KEY'], { expiresIn: 36000000 });
     }
 
     async validateUser(payload: IJwtPayload): Promise<any> {
